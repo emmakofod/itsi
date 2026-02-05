25f807ebe13dae13f607b266632e0afea38d05db
emmakofod <emskofod@gmail.com>
2026-02-05 02:31:26 -0500

    lets go challenge

--- exercises/mandatory1_part1/svarfil.py
+++ exercises/mandatory1_part1/svarfil.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+"""
+How to access challenge
+
+game=challenge.client(ip_address="cybergame.dk", port=29594)
+game.login("emma673r", "ITSI-F26")
+game.question(1)
+game.data
+
+"""
